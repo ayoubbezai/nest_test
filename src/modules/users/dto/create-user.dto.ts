1649d6b34bd1aeb70f94141dcff18dffb575974d
ayoubbezai <bezaiayoub8@gmail.com>
2025-12-01 00:14:20 +0100

    init

---
+++ src/modules/users/dto/create-user.dto.ts
@@ -0,0 +1,12 @@
+import {IsEmail , IsString, MinLength} from 'class-validator';
+
+export class CreateUserDto {
+    @IsString()
+    @MinLength(3)
+    name:string;
+
+    //email
+    @IsEmail()
+    email : string
+    
+}
